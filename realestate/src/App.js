501b7dfb520e31199451824668daa20e1a77a589
AnochAno <anothankes876@gmail.com>
2024-03-26 16:54:01 +0530

    react-Routes

--- realestate/src/App.js
+++ realestate/src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/navbar';
 import  About  from './Components/about';
 import Services from './Components/services';
-import Pricing from './Components/pricing'
+import Pricing from './Components/pricing';
 
 function App() {
   return (
     <div className="App">
-    <Navbar/>
+  
     <Router>
       
-        <Switch>
+        <Routes>
           <Route exact path='/' Component={About}/>
           <Route path='/about' Component={About}/>
           <Route path='/service' Component={Services}/>
           <Route path='/pricing' Component={Pricing}/>
-        </Switch>
+        </Routes>
       
     </Router>
      
